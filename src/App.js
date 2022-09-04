ad85443d99f703cd2ab960fd0cb9232cbabb14aa
IamChetang <86854700+IamChetang@users.noreply.github.com>
2022-09-04 21:59:40 +0530

    rakesh

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React with chetan and pooja
+          Learn React with rakesh
         </a>
       </header>
     </div>
