75230b0e198e8a603c6025c096418c416b23f412
IamChetang <86854700+IamChetang@users.noreply.github.com>
2022-09-05 17:47:57 +0530

    Navbar design and router added

---
+++ src/Components/Main.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Main.scss";
+import Startpage from "./Startpage/Startpage";
+
+const Main = () => {
+  return (
+    <>
+      <Startpage />
+    </>
+  );
+};
+
+export default Main;
