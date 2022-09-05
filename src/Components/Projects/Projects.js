805785d51caebd78ad643cdcc0cc5ef9c6538ed8
IamChetang <86854700+IamChetang@users.noreply.github.com>
2022-09-05 17:55:19 +0530

    added

--- src/Components/Projects/Projects.js
+++ src/Components/Projects/Projects.js
@@ -2,5 +2,11 @@ import React from "react";
 import "./Projects.scss";
 
 export const Projects = () => {
-  return <div>Projects</div>;
+  return (
+    <div>
+      <div className="container-fluid">
+        <h1>Projects</h1>
+      </div>
+    </div>
+  );
 };
